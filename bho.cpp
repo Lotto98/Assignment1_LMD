02ac237ec7f75feec57a07a4b0c05a68985f757d
MICHELE LOTTO <875922@stud.unive.it>
2023-03-02 20:21:05 +0100

    prove

--- bho.cpp
+++ bho.cpp
@@ -4,24 +4,64 @@
 
 using namespace std;
 
-#define N 10
+#define N 5
 
-int main(int argc, char const *argv[])
-{
-    array<vector<int>, N> matrix;
 
-    for (size_t i = 0; i < N; i++)
-    {
-        matrix[i].insert(matrix[i].begin(),N-i,0);
+class TriangularMatrix{
+    private:
+        size_t static const n=N-1; //actual size
+
+        array<vector<bool>, n> matrix;
+    public:
+        TriangularMatrix(/* args */);
+        void print_row(int i);
+        void pretty_print();
+
+};
+
+TriangularMatrix::TriangularMatrix(/* args */){
+    for (size_t i = 0; i < n; i++){
+        matrix[i].insert(matrix[i].begin(),n-i,0);
     }
+}
 
-    for (size_t i = 0; i < N; i++)
-    {
+void TriangularMatrix::print_row(int row){
+    if (row>n){
+        throw invalid_argument( "received invalid row index" );
+    }
+    else{
+        for (auto j: matrix[row]){
+            cout << j << ' ';
+        } 
+    }
+}
+
+void TriangularMatrix::pretty_print(){
+    for (size_t i = 0; i < n; i++){
+
+        for (size_t k=0; k<n-matrix[i].size()+1; k++){
+            cout<<"* ";
+        }
+
         for (auto j: matrix[i]){
             cout << j << ' ';
         }
         cout<<endl;
     }
 
+    for (size_t i = 0; i < N; i++){
+        cout<<"* ";
+    }    
+
+    cout<<endl;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    auto tm=TriangularMatrix();
+    //tm.pretty_print();
+
+    tm.print_row(-1);
     return 0;
 }
